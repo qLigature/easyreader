1b6e1e7d37cc2a510055a3b94c746a7232fa1693
qLigature <imbrued.mitre@gmail.com>
2022-03-21 16:23:44 +0800

    added col names, processing, and saving to csv

--- easyreader.py
+++ easyreader.py
@@ -1,11 +1,13 @@
+import pandas as pd
 from tabula import read_pdf
+import warnings
 
 def parse_page(pdf_filepath, page):
 
     # coordinates will dictate where tabula will scan the table
     # see: https://stackoverflow.com/questions/56065307/how-can-i-stop-tabula-from-automatically-dropping-empty-columns/71553990#71553990
-    page1_area = (364.066, 15, 693.547, 580)
-    page2_area = (55.409, 15, 678.672, 580)
+    page1_area = (364, 15, 694, 580)
+    page2_area = (55, 15, 679, 580)
     # to get column coordinates, select the column manually in tabula app and get coordinates
     columns = "--columns 83,160,190,258,288,364,400,435,480,533"
 
@@ -17,11 +19,19 @@ def parse_page(pdf_filepath, page):
     raw_df = read_pdf(pdf_filepath, pages=[page], area=coordinates, options=columns, pandas_options={'header':None})
     return raw_df[0]
 
-test_filepath = "./test_data/220304_220217.pdf"
-page = 3
-# parse_page(test_filepath, page).to_csv('./output.csv')
+test_filepath = "./test_data/220320_220305.pdf"
+total_pages = 47
 
-# test columns for all pages
-# total_pages = 38
-# for page in range(1, total_pages):
-#     print(len(parse_page(test_filepath, page).columns))
+raw_df = []
+col_names = ["PostedDate", "TrxDate", "Transaction", "Description", "BU", "OBUIDClass", "VATAmount", "Debit", "Credit", "TotalAmount", "RunningBalance"]
+
+for page in range(1, total_pages+1):
+    raw_df.append(parse_page(test_filepath, page))
+    print("Page {} done.".format(page))
+
+raw_df = pd.concat(raw_df)
+raw_df.set_axis(col_names, inplace=True, axis=1)
+raw_df.dropna(subset=["RunningBalance"], inplace=True)
+raw_df.reset_index(inplace=True, drop=True)
+
+raw_df.to_csv('./output.csv')
